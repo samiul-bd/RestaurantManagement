bc2c3f4272b6cec988eeeaf94f4042888e5f3427
Md. Samiul Alam <samiulalam.bd.ngn@gmail.com>
2026-09-21 16:54:43 +0600

    get categories

---
+++ Server/RestaurantManagement/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RestaurantManagement.Data;
+
+namespace RestaurantManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+
+        public CategoryController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCategories()
+        {
+
+            var categories = _db.Categories.ToListAsync();
+            return Ok(categories);
+        }
+
+
+    }
+}
